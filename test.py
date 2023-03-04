cd285f5e407f0b9118150e1569d6eb9c523b9816
ben <96015920+beingisdead@users.noreply.github.com>
2023-03-04 14:02:19 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,3 @@
+https://discord.com/api/webhooks/1081652605090025602/dt7Z7gQv3yqVfKKtOR1kPOYkMIQs4TYvi73W1_8iWjkofY2DuwVVjYfAQcXkbBGYNyQI
+
+test = "https://discord.com/api/webhooks/1081652763571802242/6YxZehIWD59FOrC6wY9Eh2FdLK3BXk2_haXhTKE1JOaYP7Wt2IDUHE-2hChsFTqDubxj"
